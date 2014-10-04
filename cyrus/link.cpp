ec344ab309968656b520489244800a9771158b52
qimengcheng <laputa@foxmail.com>
2014-10-04 20:44:38 +0800

    a1

---
+++ cyrus/link.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int a[] = { 1, 2, 1, 3, 6, 45, 23, 776, 21, 34, 6776, 8, 436546, 68, 425,56 };
+typedef struct LinkedList
+{
+	int con;
+	LinkedList* next;	
+}node;
+
+node* create()
+{
+	node *head, *p, *q;
+	head = (node*)(malloc(sizeof(node)));
+	q = head;
+	for (int i = 1; i < sizeof (a) / 4; ++i)
+	{
+		p = (node*)(malloc(sizeof(node)));
+		q->next = p;
+		q->con = a[i];
+		q = p;
+	}
+	return head;
+
+
+
+}
+int main() 
+{
+	node* head = create();
+	node* p = head;
+	for (int i = 1; p->next != NULL; i++)
+	{
+		if (i%5==4)
+		{
+		node* temp = p->next;
+		cout <<temp->con
+				<< endl;
+			p->next = p->next->next;
+			free(temp);
+			
+		}
+		else
+		{
+			p = p->next;
+		}
+	}
+	getchar();
+}
